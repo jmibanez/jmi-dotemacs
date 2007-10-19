29b1d8b13cf7cb34a3aab53defe901371bacd9e1
JM Ibanez <jmibanez@orangeandbronze.com>
2007-10-20 06:36:42 +0800

    Added config for Trac wiki support.

---
+++ trac-wiki.init.el
@@ -0,0 +1,8 @@
+;; Trac-wiki editing
+
+(require 'trac-wiki)
+(setq trac-projects
+      '(("rover-contest"
+         :endpoint "https://issues.orangeandbronze.com/rover-contest/xmlrpc")
+        ("onb-training"
+         :endpoint "https://issues.orangeandbronze.com/training/xmlrpc")))

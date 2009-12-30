e74ff7bb6232ebf7fa95b34d1e8b943a27f6c264
JM Ibanez <jmibanez@orangeandbronze.com>
2009-12-30 12:55:59 +0800

    Add BBDB to load path.
    On mollymillions, we don't have BBDB as part of the dist, so have to
    include a separate copy instead.

--- loadpaths.el
+++ loadpaths.el
@@ -2,6 +2,7 @@
 (add-to-list 'load-path "~/elisp")
 (add-to-list 'load-path "~/elisp/lj-update")
 (add-to-list 'load-path "~/elisp/ee")
+(add-to-list 'load-path "~/elisp/bbdb")
 
 (add-to-list 'load-path "~/elisp/jde/lisp")
 (add-to-list 'load-path "~/elisp/w3m")

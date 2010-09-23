2eda63abd5a9c6edbb6e584799d431959bfc7086
JM Ibanez <jmibanez@gmail.com>
2010-09-24 03:05:22 +0800

    Use a different custom script to run SQL*Plus

--- custom.init.el
+++ custom.init.el
@@ -38,7 +38,7 @@
  '(ns-pop-up-frames nil)
  '(scheme-mit-dialect nil)
  '(scheme-program-name "mzscheme")
- '(sql-oracle-program "~/scripts/ssh-sqlplus.sh")
+ '(sql-oracle-program "~/scripts/mac-sqlplus.sh")
  '(transient-mark-mode t)
  '(w3m-command "/opt/local/bin/w3m")
  '(w3m-default-display-inline-images t)

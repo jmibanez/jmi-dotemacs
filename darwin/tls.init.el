99ab629ece99ec2a2c85a1f67a02e1a6b54f787a
JM Ibanez <jabz@amazon.com>
2018-11-01 12:51:51 +1100

    Don't hardcode path to openssl
    This allows for the safe upgrade of the (unlinked) openssl package in
    Homebrew

--- darwin/tls.init.el
+++ darwin/tls.init.el
@@ -3,6 +3,16 @@
 ;;; Commentary:
 
 ;;; Code:
-(setq tls-program '("/usr/local/Cellar/openssl/1.0.2o_1/bin/openssl s_client -connect %h:%p -no_ssl2 -ign_eof"))
+
+;; We assume Homebrew exists on this Mac, and that openssl is not
+;; linked. Use all the versions in reverse order (try latest first)
+(setq tls-program
+      (mapcar (lambda (openssl-base)
+                (concat openssl-base
+                        "/bin/openssl"
+                        " s_client -connect %h:%p -no_ssl2 -ign_eof"))
+              (reverse
+               (directory-files "/usr/local/Cellar/openssl/" t
+                                "^\\([^.]\\|\\.[^.]\\|\\.\\..\\)" nil))))
 
 ;;; tls.init.el ends here

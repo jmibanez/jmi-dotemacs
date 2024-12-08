b800de2fd38f5c1568dbb92c532142841f679398
JM Ibanez <jm@jmibanez.com>
2024-12-08 18:44:59 +1100

    Bind <f8> c c to projectile-compile-project instead of compile.
    Much better, since it respects what should be done on a
    project-to-project basis.

--- keybindings.init.el
+++ keybindings.init.el
@@ -55,7 +55,7 @@
 (bind-keys :prefix-map jmi/my-jump-keys-map
            :prefix "<f8>"
 
-           ("c c"      . compile)
+           ("c c"      . projectile-compile-project)
            ("f p"      . jmi/toggle-http-proxy)
            ("f f"      . jmi/do-mail-sync))
 

75367c5c6a02d789c25a314f725f5a8d41bf15f0
JM Ibanez <jm@jmibanez.com>
2024-12-12 22:39:20 +1100

    Enable use of macOS Keychain for secrets.
    Recent auth-source allows the use of the macOS Keychain as an
    auth-source in `auth-sources`, so enable that -- I don't want to have
    to use ~/.authinfo.gpg (and have to do the GPG dance), plus having it
    in the Keychain makes it possible to potentially share these across my
    devices.

--- darwin/000.init.el
+++ darwin/000.init.el
@@ -12,6 +12,10 @@
 ;; Configure system-packages-package-manager, ensure we have it set to brew
 (setq system-packages-package-manager 'brew)
 
+;; Add macOS keychains to auth-sources
+(add-to-list 'auth-sources 'macos-keychain-internet)
+(add-to-list 'auth-sources 'macos-keychain-generic)
+
 ;; We mark that we've provided platform-specific initialization
 (provide 'jmi-init-platform)
 

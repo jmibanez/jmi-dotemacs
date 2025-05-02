d3abe13aeca317c1cbaa3d7207d9b58b7986ee24
JM Ibanez <jm@jmibanez.com>
2025-05-02 23:02:06 +1000

    Refactor: move keybindings into a use-package block for consistency
    Make things a bit more consistent and move my custom keybinding stuff
    into a use-package block.

--- keybindings.init.el
+++ keybindings.init.el
@@ -2,65 +2,64 @@
 
 ;;; Commentary:
 
-;;; This adds a custom keymap for custom functions; by default, I've
-;;; bound it to <f8>.  This supersedes my old keybindings.el config
+;;; Keybindings-only config, This adds a custom keymap for custom
+;;; functions; by default, I've bound it to <f8>. Also provides a few
+;;; custom functions to DWIM (e.g. for scope-based find-file)
 
 ;;; Code:
 
+(use-package bind-key
+  :ensure nil
+  :demand t
 
-;; To avoid any weirdness, we don't use use-package here --
-;; use-package depends on bind-key, and we're using bind-key pretty
-;; directly anyway
-(require 'bind-key)
+  :config
+  ;; User function: Toggle http_proxy in process-environment. Used so I
+  ;; can go online etc. via a tunneled connection.
 
-;; User function: Toggle http_proxy in process-environment. Used so I
-;; can go online etc. via a tunneled connection.
+  (defun jmi/toggle-http-proxy ()
+    "Toggle the value/existence of http_proxy and the TSOCKS proxy."
+    (interactive)
+    (if (getenv "http_proxy")
+        (setenv "http_proxy" nil)
+      (setenv "http_proxy" "http://localhost:8888"))
+    (jmi/do-toggle-tsocks)
+    (message (let ((proxy (getenv "http_proxy")))
+               (cond (proxy)
+                     ("ENV: Unset proxy")))))
 
-(defun jmi/toggle-http-proxy ()
-  "Toggle the value/existence of http_proxy and the TSOCKS proxy."
-  (interactive)
-  (if (getenv "http_proxy")
-      (setenv "http_proxy" nil)
-    (setenv "http_proxy" "http://localhost:8888"))
-  (jmi/do-toggle-tsocks)
-  (message (let ((proxy (getenv "http_proxy")))
-             (cond (proxy)
-                   ("ENV: Unset proxy")))))
-
-;; FIXME: Currently broken -- need to rewrite to work on either macOS or Linux
-(defun jmi/do-toggle-tsocks ()
-  "Toggle LD_PRELOAD for tsocks."
-  (if (and t;; (getenv "LD_PRELOAD") ;; Stupid hack to get LD_PRELOAD synched with http_proxy
-           (getenv "http_proxy"))
-      (setenv "LD_PRELOAD" "/usr/lib/libtsocks.so")
-    (setenv "LD_PRELOAD" nil)))
+  ;; FIXME: Currently broken -- need to rewrite to work on either macOS or Linux
+  (defun jmi/do-toggle-tsocks ()
+    "Toggle LD_PRELOAD for tsocks."
+    (if (and t;; (getenv "LD_PRELOAD") ;; Stupid hack to get LD_PRELOAD synched with http_proxy
+             (getenv "http_proxy"))
+        (setenv "LD_PRELOAD" "/usr/lib/libtsocks.so")
+      (setenv "LD_PRELOAD" nil)))
 
 
-(defun jmi/projectile-find-file-or-org-roam-node-find-dwim ()
-  "Either do projectile-find-file or or do org-roam-find; DWIM."
-  (interactive)
+  (defun jmi/projectile-find-file-or-org-roam-node-find-dwim ()
+    "Either do projectile-find-file or or do org-roam-find; DWIM."
+    (interactive)
 
-  (if (not (fboundp 'projectile-find-file))
-      (error "Projectile is not loaded.")
+    (if (not (fboundp 'projectile-find-file))
+        (error "Projectile is not loaded.")
 
-    (if (and (fboundp 'jmi/org-roam-buffer-p)
-             (eq major-mode 'org-mode)
-             (jmi/org-roam-buffer-p))
-        (org-roam-node-find)
+      (if (and (fboundp 'jmi/org-roam-buffer-p)
+               (eq major-mode 'org-mode)
+               (jmi/org-roam-buffer-p))
+          (org-roam-node-find)
 
-      ;; Else, default to projectile-find-file
-      (projectile-find-file-dwim))))
+        ;; Else, default to projectile-find-file
+        (projectile-find-file-dwim))))
 
-;; Our shortcut map
-(bind-keys :prefix-map jmi/my-jump-keys-map
-           :prefix "<f8>"
+  ;; Our shortcut map
+  (bind-keys :prefix-map jmi/my-jump-keys-map
+             :prefix "<f8>"
 
-           ("c c"      . projectile-compile-project)
-           ("f p"      . jmi/toggle-http-proxy)
-           ("f f"      . jmi/do-mail-sync))
+             ("c c"      . projectile-compile-project)
+             ("f p"      . jmi/toggle-http-proxy)
+             ("f f"      . jmi/do-mail-sync))
 
-(global-set-key [(super t)] #'jmi/projectile-find-file-or-org-roam-node-find-dwim)
-
-(provide 'jmi-keybindings)
+  :bind
+  (("s-t"   . jmi/projectile-find-file-or-org-roam-node-find-dwim)))
 
 ;;; keybindings.init.el ends here

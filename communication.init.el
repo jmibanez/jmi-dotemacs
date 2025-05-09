jmibanez/jmi-dotemacs

commit d3abe13aeca317c1cbaa3d7207d9b58b7986ee24

Refactor: move keybindings into a use-package block for consistency
Make things a bit more consistent and move my custom keybinding stuff
into a use-package block.

keybindings.init.el
@@ -2,65 +2,64 @@
 
 ;;; Commentary:
 
-;;; This adds a custom keymap for custom functions; by default, I've
-;;; bound it to <f8>.  This supersedes my old keybindings.el config
+;;; Keybindings-only config, This adds a custom keymap for custom
+;;; functions; by default, I've bound it to <f8>. Also provides a few
+;;; custom functions to DWIM (e.g. for scope-based find-file)
 
 ;;; Code:
 
+(use-package bind-key
+  :ensure nil
+  :demand t
 
-;; To avoid any weirdness, we don't use use-package here --
-;; use-package depends on bind-key, and we're using bind-key pretty
-;; directly anyway
-(require 'bind-key)
+  :config
+  ;; User function: Toggle http_proxy in process-environment. Used so I
+  ;; can go online etc. via a tunneled connection.
 
-;; User function: Toggle http_proxy in process-environment. Used so I
-;; can go online etc. via a tunneled connection.
+  (defun jmi/toggle-http-proxy ()
+    "Toggle the value/existence of http_proxy and the TSOCKS proxy."
+    (interactive)
+    (if (getenv "http_proxy")
+        (setenv "http_proxy" nil)
+      (setenv "http_proxy" "http://localhost:8888"))
+    (jmi/do-toggle-tsocks)
+    (message (let ((proxy (getenv "http_proxy")))
+               (cond (proxy)
+                     ("ENV: Unset proxy")))))
 
-(defun jmi/toggle-http-proxy ()
-  "Toggle the value/existence of http_proxy and the TSOCKS proxy."
-  (interactive)
-  (if (getenv "http_proxy")
-      (setenv "http_proxy" nil)
-    (setenv "http_proxy" "http://localhost:8888"))
-  (jmi/do-toggle-tsocks)
-  (message (let ((proxy (getenv "http_proxy")))
-             (cond (proxy)
-                   ("ENV: Unset proxy")))))
-
-;; FIXME: Currently broken -- need to rewrite to work on either macOS or Linux
-(defun jmi/do-toggle-tsocks ()
-  "Toggle LD_PRELOAD for tsocks."
-  (if (and t;; (getenv "LD_PRELOAD") ;; Stupid hack to get LD_PRELOAD synched with http_proxy
-           (getenv "http_proxy"))
-      (setenv "LD_PRELOAD" "/usr/lib/libtsocks.so")
-    (setenv "LD_PRELOAD" nil)))
+  ;; FIXME: Currently broken -- need to rewrite to work on either macOS or Linux
+  (defun jmi/do-toggle-tsocks ()
+    "Toggle LD_PRELOAD for tsocks."
+    (if (and t;; (getenv "LD_PRELOAD") ;; Stupid hack to get LD_PRELOAD synched with http_proxy
+             (getenv "http_proxy"))
+        (setenv "LD_PRELOAD" "/usr/lib/libtsocks.so")
+      (setenv "LD_PRELOAD" nil)))
 
 
-(defun jmi/projectile-find-file-or-org-roam-node-find-dwim ()
-  "Either do projectile-find-file or or do org-roam-find; DWIM."
-  (interactive)
+  (defun jmi/projectile-find-file-or-org-roam-node-find-dwim ()
+    "Either do projectile-find-file or or do org-roam-find; DWIM."
+    (interactive)
 
-  (if (not (fboundp 'projectile-find-file))
-      (error "Projectile is not loaded.")
+    (if (not (fboundp 'projectile-find-file))
+        (error "Projectile is not loaded.")
 
-    (if (and (fboundp 'jmi/org-roam-buffer-p)
-             (eq major-mode 'org-mode)
-             (jmi/org-roam-buffer-p))
-        (org-roam-node-find)
+      (if (and (fboundp 'jmi/org-roam-buffer-p)
+               (eq major-mode 'org-mode)
+               (jmi/org-roam-buffer-p))
+          (org-roam-node-find)
 
-      ;; Else, default to projectile-find-file
-      (projectile-find-file-dwim))))
+        ;; Else, default to projectile-find-file
+        (projectile-find-file-dwim))))
 
-;; Our shortcut map
-(bind-keys :prefix-map jmi/my-jump-keys-map
-           :prefix "<f8>"
+  ;; Our shortcut map
+  (bind-keys :prefix-map jmi/my-jump-keys-map
+             :prefix "<f8>"
 
-           ("c c"      . projectile-compile-project)
-           ("f p"      . jmi/toggle-http-proxy)
-           ("f f"      . jmi/do-mail-sync))
+             ("c c"      . projectile-compile-project)
+             ("f p"      . jmi/toggle-http-proxy)
+             ("f f"      . jmi/do-mail-sync))
 
-(global-set-key [(super t)] #'jmi/projectile-find-file-or-org-roam-node-find-dwim)
-
-(provide 'jmi-keybindings)
+  :bind
+  (("s-t"   . jmi/projectile-find-file-or-org-roam-node-find-dwim)))
 
 ;;; keybindings.init.el ends here

commit 9b3f400a3196d50a6cfb168d61afefc24678e053

Fix keybinding breakage
I broke my prefix map in d3abe13a, as I forgot to `(provide
'jmi-keybindings)` which in turn suppressed loading a few
packages (including elfeed).

As I actually created that pseudo feature in 47472e6d as a way to
ensure that the prefix map exists before other use-package
declarations depend on it, I thought it best to just bite the bullet
and move the definition of the prefix map to main.el -- ensuring it
will always be loaded first anyway, so all other use-package
declarations can expect it to be there. This has the added bonus of
not needing the pseudo feature `jmi-keybindings`, cleaning things up a
bit.

communication.init.el
@@ -64,9 +64,7 @@
                ("S-<f7>"  .  jmi/elfeed-search-bookmark-all)
                ("*"       .  jmi/elfeed-jump-to-latest-unread-entries))
          (:map elfeed-show-mode-map
-               ("S-<f7>"  .  jmi/elfeed-show-bookmark-entry)))
-
-  :after jmi-keybindings)
+               ("S-<f7>"  .  jmi/elfeed-show-bookmark-entry))))
 
 
 (use-package circe
@@ -169,8 +167,7 @@
   :hook
   ((message-send             .  jmi/mimedown))
 
-  :ensure nil
-  :after (jmi-keybindings))
+  :ensure nil)
 
 (use-package mbsync
   :config
@@ -210,7 +207,7 @@
 
   :hook ((mbsync-exit       .  jmi/scan-mail-and-news)
          (gnus-startup      .  gnus-demon-init))
-  :after (jmi-keybindings gnus))
+  :after (gnus))
 
 (use-package alert
   :config

keybindings.init.el
@@ -51,15 +51,11 @@
         ;; Else, default to projectile-find-file
         (projectile-find-file-dwim))))
 
-  ;; Our shortcut map
-  (bind-keys :prefix-map jmi/my-jump-keys-map
-             :prefix "<f8>"
-
-             ("c c"      . projectile-compile-project)
-             ("f p"      . jmi/toggle-http-proxy)
-             ("f f"      . jmi/do-mail-sync))
-
   :bind
-  (("s-t"   . jmi/projectile-find-file-or-org-roam-node-find-dwim)))
+  (("s-t"   . jmi/projectile-find-file-or-org-roam-node-find-dwim)
+   (:map jmi/my-jump-keys-map
+         ("c c"      . projectile-compile-project)
+         ("f p"      . jmi/toggle-http-proxy)
+         ("f f"      . jmi/do-mail-sync))))
 
 ;;; keybindings.init.el ends here

main.el
@@ -64,6 +64,11 @@
     (concat jmi/my-emacs-init-path
             sanitized-platform-dir)))
 
+(require 'bind-key)
+
+(bind-keys :prefix-map jmi/my-jump-keys-map
+           :prefix "<f8>")
+
 ;; If there are any customizations per-machine, per-user, load them
 ;; first, so that our config can refer to them
 (mapc 'load

snippets.init.el
@@ -35,10 +35,7 @@
   (setq yas-snippet-dirs
         '("~/.emacs.d/snippets"))
 
-  (add-to-list 'yas-prompt-functions #'shk-yas/helm-prompt)
-
-  :after
-  helm jmi-keybindings)
+  (add-to-list 'yas-prompt-functions #'shk-yas/helm-prompt))
 
 
 (use-package yasnippet-snippets
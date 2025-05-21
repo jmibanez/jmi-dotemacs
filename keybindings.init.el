748afd28ae1a7e5a5cd12223a1f6e6289ad7d792
JM Ibanez <jm@jmibanez.com>
2025-05-22 01:42:45 +1000

    Excise jmi/do-toggle-tsocks and jmi/toggle-http-proxy
    I used to have this for toggling tsocks on my Linux environment when I
    needed to tunnel my connections via a SSH-based SOCKS proxy. However,
    I no longer need to do this, and I've long stopped installing
    tsocks.
    
    Additionally, I used to use jmi/toggle-http-proxy to toggle my HTTP
    proxy settings as inherited via process-environment. There are better
    ways now, such as setting `url-proxy-services`.
    
    Kill it for now. If I need it later, I can resurrect it from Git
    history.

--- keybindings.init.el
+++ keybindings.init.el
@@ -12,29 +12,6 @@
   :ensure nil
 
   :config
-  ;; User function: Toggle http_proxy in process-environment. Used so I
-  ;; can go online etc. via a tunneled connection.
-
-  (defun jmi/toggle-http-proxy ()
-    "Toggle the value/existence of http_proxy and the TSOCKS proxy."
-    (interactive)
-    (if (getenv "http_proxy")
-        (setenv "http_proxy" nil)
-      (setenv "http_proxy" "http://localhost:8888"))
-    (jmi/do-toggle-tsocks)
-    (message (let ((proxy (getenv "http_proxy")))
-               (cond (proxy)
-                     ("ENV: Unset proxy")))))
-
-  ;; FIXME: Currently broken -- need to rewrite to work on either macOS or Linux
-  (defun jmi/do-toggle-tsocks ()
-    "Toggle LD_PRELOAD for tsocks."
-    (if (and t;; (getenv "LD_PRELOAD") ;; Stupid hack to get LD_PRELOAD synched with http_proxy
-             (getenv "http_proxy"))
-        (setenv "LD_PRELOAD" "/usr/lib/libtsocks.so")
-      (setenv "LD_PRELOAD" nil)))
-
-
   (defun jmi/in-org-roam-related-buffer-p ()
     (or (and (fboundp 'jmi/org-roam-buffer-p)
              (jmi/org-roam-buffer-p))

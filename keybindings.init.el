cd2623fa17aa1fc33026094defee6be3bde07ba4
JM Ibanez <jm@jmibanez.com>
2025-05-21 23:31:48 +1000

    Bind C-& as an alternate Super key
    This allows me to use my Model M keyboard, or any older keyboard which
    doesn't have a spare key for Super for that matter, since I rely too
    much on Super nowadays, and the default event-apply-super-modifier
    binding of C-x @ s is too cumbersome to use frequently.

--- keybindings.init.el
+++ keybindings.init.el
@@ -53,6 +53,10 @@
         ;; Else, default to projectile-find-file
         (projectile-find-file-dwim))))
 
+  ;; On keyboards which don't have Super (e.g. my Model M) bind C-& to
+  ;; event-apply-super-modifier so I can reach my bindings that use Super
+  (define-key function-key-map (kbd "C-&") 'event-apply-super-modifier)
+
   :bind
   (("s-t"   . jmi/projectile-find-file-or-org-roam-node-find-dwim)
    (:map jmi/my-jump-keys-map

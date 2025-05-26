b848960db20a17077cca873d02b122219ea75bdf
JM Ibanez <jm@jmibanez.com>
2025-05-27 01:20:06 +1000

    Remove advice for alert terminal notifier
    I don't use it at all since moving to osx-notifier, so might as well
    get rid of it.
    
    TODO: I need to do some platform checks here for alert-default-style.

--- communication.init.el
+++ communication.init.el
@@ -219,24 +219,7 @@
 (use-package alert
   :defer t
   :config
-  (setq alert-default-style 'osx-notifier)
-
-  ;; We need to replace the -appIcon switch with -contentImage, as
-  ;; terminal-notifier can't replace the notification icon
-  (defun jmi/alert-notifier-notify (info)
-    (if alert-notifier-command
-        (let ((args
-               (list "-title"   (alert-encode-string (plist-get info :title))
-                     "-contentImage" (or (plist-get info :icon) alert-notifier-default-icon)
-                     "-message" (alert-encode-string (plist-get info :message)))))
-          (apply #'call-process alert-notifier-command nil nil nil args))
-      (alert-message-notify info)))
-
-  (advice-add 'alert-notifier-notify :override
-              #'jmi/alert-notifier-notify)
-
-
-  :ensure-system-package terminal-notifier)
+  (setq alert-default-style 'osx-notifier))
 
 (use-package gnus-desktop-notify
   :defer t
